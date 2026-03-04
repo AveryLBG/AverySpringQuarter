43d60791c5a18913788787b8579c03083d53f6ef
Avery Garner <avery.garner@hgs.hiddengeniusproject.org>
2026-03-04 15:34:21 -0800

    Re-added Mobile Dev Project

---
+++ Mobile Dev Folder/SpringPod/SpringPod/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpringPod
+//
+//  Created by Avery on 3/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
