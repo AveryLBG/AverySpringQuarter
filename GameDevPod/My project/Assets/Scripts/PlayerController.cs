c4b044eac74608d3026a880079e8b58978acd56e
Avery Garner <avery.garner@hgs.hiddengeniusproject.org>
2026-03-25 18:17:38 -0700

    THE BALL MOVES!!!!!!

--- GameDevPod/My project/Assets/Scripts/PlayerController.cs
+++ GameDevPod/My project/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Create variables
+    [SerializeField, Tooltip("The player's Rigidbody, can be used to apply movement.") ]
+
+    private Rigidbody rb;
+    
+    [SerializeField, Tooltip("Movement speed multiplier") ]
+
+    private float moveSpeed = 7f;
+
+    private void Awake() 
     {
-        
+        //Initialize Variables
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
-        
+        //Gather input from player
+        float xInput = Input.GetAxis("Horizontal");// Left & Right movement
+        float zInput = Input.GetAxis("Vertical");// Forward & Back Movement
+        //Convert input into move direction
+        Vector3 moveDir = new Vector3(xInput, 0, zInput);
+
+    
+        //Apply Input into movement on the player character.
+        rb.AddForce(moveDir * moveSpeed * Time.deltaTime ,ForceMode.Impulse);
     }
 }
